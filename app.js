b928ae9cbfad05196571bef37eb5e46924ee0c5b
sisidovski <arerreee@gmail.com>
2017-12-11 11:27:09 +0900

    WIP

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const path = require('path');
+const engines = require('consolidate');
 const app = express();
 
+// app.set('views', path.join(__dirname, 'views'));
+// app.engine('html', engines.jade);
+// app.set('view engine', '.html');
+app.use(express.static(path.join(__dirname, 'views')));
+
 app.get('/', (req, res) => {
-  res.send('<a href="/prerender-with-header">with header</p>');
+  res.render('index-link-tag.html');
 });
 
 app.get('/prerender-with-header', (req, res) => {
   res.set('Link', '</prerender-resource>; rel="prerender";');
-  res.render('index');
+  res.send('index-link-header.html');
 });
 
 app.get('/prerender-resource', (req, res) => {
